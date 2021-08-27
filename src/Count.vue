bc3c059ed5faf0e3239021998a5feb5dfed7fa54
clemvnt <clemvnt@gmail.com>
2021-08-27 20:14:43 +0200

    use ref sugar

--- src/Count.vue
+++ src/Count.vue
@@ -1,23 +1,13 @@
 <script setup>
-import { ref } from 'vue'
-
 const props = defineProps({
   count: { type: Number, default: 5 }
 })
 
-const countValue = ref(props.count)
-
-function increment() {
-  countValue.value++
-}
-
-function decrement() {
-  countValue.value--
-}
+let value = $ref(props.count)
 </script>
 
 <template>
-  <button id="decr" @click="decrement">--</button>
-  <span>{{ countValue }}</span>
-  <button id="incr" @click="increment">++</button>
+  <button id="decr" @click="value--">--</button>
+  <span>{{ value }}</span>
+  <button id="incr" @click="value++">++</button>
 </template>
